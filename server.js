56e73c3df9ed536f14006a051420ba0e87e9c298
Brisilda Bushi <b.brisilda1552@gmail.com>
2021-09-08 21:31:30 +0200

    Adding backend for the contact form

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer")
+
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(express.static("src"));
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/index.html")
+})
+
+app.post("/", (req, res) => {
+  console.log(req.body)
+
+  const transporter = nodemailer.createTransport({
+    host: "mail.coding-school.org",
+    port: 465,
+    auth: {
+      user: "fbw46-2@coding-school.org",
+      pass: "fbw46-2",
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailOptions = {
+    from: "fbw46-2@coding-school.org",
+    to: "b.brisilda1552@gmail.com",
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: "Message:"+ "\n" + req.body.message
+  }
+  
+  transporter.sendMail(mailOptions, function (error, info) {
+    if(error){
+      console.log(error)
+      res.send("error")
+    }else{
+      console.log("Email sent: " + info.response)
+      res.send("success")
+    }
+  })
+})
+  
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- src/js/main.js
+++ src/js/main.js
@@ -196,3 +196,40 @@ themeButton.addEventListener("click", () => {
 //   if(this.scrollY >= 560) navShow.classList.add("show-scroll"); else navShow.classList.remove("show-scroll");
 // }
 // window.addEventListener("navshow", navShow)
+
+// -----------------------Set up email sender-------------------------------------------
+const contactForm = document.querySelector(".contact__form");
+let name = document.getElementById("name")
+let email = document.getElementById("email")
+let subject = document.getElementById("subject")
+let message = document.getElementById("message")
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value
+  }
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.onload = function(){
+    console.log(xhr.responseText);
+    if(xhr.responseText === "success"){
+      alert("Email Sent!");
+      name.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+    } else {
+      alert("Something went wrong!")
+    }
+  }
+
+  xhr.send(JSON.stringify(formData))
+
+})
