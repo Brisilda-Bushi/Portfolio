7c4bae0230c24f920c53097285e539e3e77b83b8
Brisilda Bushi <b.brisilda1552@gmail.com>
2021-09-10 12:27:44 +0200

    Adding new email process for backend, using my own account

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer")
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,11 +18,10 @@ app.post("/", (req, res) => {
   console.log(req.body)
 
   const transporter = nodemailer.createTransport({
-    host: "mail.coding-school.org",
-    port: 465,
+    service: "gmail",
     auth: {
-      user: "fbw46-2@coding-school.org",
-      pass: "fbw46-2",
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
     },
     tls: {
       // do not fail on invalid certs
@@ -30,7 +30,7 @@ app.post("/", (req, res) => {
   });
 
   const mailOptions = {
-    from: "fbw46-2@coding-school.org",
+    from: "web.brisilda@gmail.com",
     to: "b.brisilda1552@gmail.com",
     subject: `Message from ${req.body.email}: ${req.body.subject}`,
     text: "Message:"+ "\n" + req.body.message
