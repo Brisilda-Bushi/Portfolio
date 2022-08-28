f4cc70fd0545d46425146f03eaad95694a90b406
Brisilda Bushi <b.brisilda1552@gmail.com>
2022-08-28 15:12:07 +0200

    connecting with A2hosting

--- server.js
+++ server.js
@@ -1,53 +1,53 @@
 const express = require("express");
 const app = express();
-const nodemailer = require("nodemailer")
-require("dotenv").config({path: "./config.env"});
+const nodemailer = require("nodemailer");
+require("dotenv").config({ path: "./config.env" });
 
 const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.static("src"));
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html")
-})
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.post("/", (req, res) => {
-  console.log(req.body)
+    console.log(req.body);
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-    tls: {
-      // do not fail on invalid certs
-      rejectUnauthorized: false,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        host: "mail.brisilda-bushi.com",
+        port: 465,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false,
+        },
+    });
 
-  const mailOptions = {
-    from: "web.brisilda@gmail.com",
-    to: "b.brisilda1552@gmail.com",
-    subject: `Message from ${req.body.email}: ${req.body.subject}`,
-    text: "Message:"+ "\n" + req.body.message
-  }
-  
-  transporter.sendMail(mailOptions, function (error, info) {
-    if(error){
-      console.log(error)
-      res.send("error")
-    }else{
-      console.log("Email sent: " + info.response)
-      res.send("success")
-    }
-  })
-})
-  
+    const mailOptions = {
+        from: "portfolio@brisilda-bushi.com",
+        to: "b.brisilda1552@gmail.com",
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: "Message:" + "\n" + req.body.message,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            res.send("error");
+        } else {
+            console.log("Email sent: " + info.response);
+            res.send("success");
+        }
+    });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
